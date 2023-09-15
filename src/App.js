078f3d72352f194f18f068b2700263f1a22e7f6c
bohdanahostynska <bohdana271@gmail.com>
2023-09-15 07:19:06 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../src/components/Header";
+
+import Main from "../src/components/Main";
+
+import Footer from "../src/components/Footerr";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
